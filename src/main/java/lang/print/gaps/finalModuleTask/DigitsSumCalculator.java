6a160b8168857190177bbf69224ad096ed407f75
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-03 18:50:10 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+    int result = 0;
+	    result += number%10;
+	     number = number/10;
+	    result += number%10;
+	     number = number/10;
+	     result += number%10;
+	     number = number/10;
+	     result += number%10;
+	     number = number/10;
+	    System.out.println(result);
     }
 }
